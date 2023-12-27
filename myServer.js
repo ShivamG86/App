4b0c971b5aa0af0013cc062526412aea218c854a
ShivamG86 <151511259+ShivamG86@users.noreply.github.com>
2023-12-27 13:26:54 +0530

    Update myServer.js

--- myServer.js
+++ myServer.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const axios = require("axios");
 const cors = require("cors");
-
+const axios = require("axios");
 
 const app = express();
-const port = process.env.PORT||3001;
+const port = 3001;
 
 app.use(express.json());
 app.use(cors());
@@ -16,15 +15,22 @@ app.post("/api/callEndpoint", async (req, res) => {
     let responseData;
     let responseStatus;
 
-    if (method === "GET") { 
+    if (method === "GET") {
+      try {
       const response = await axios.get(fetchURL);
       responseData = response.data;
-      responseStatus = response.status
-    
+      responseStatus = response.status;
+      }catch (error) {
+        responseData = error;
+      }
     } else if (method === "POST") {
+      try {
       const response = await axios.post(fetchURL, data);
       responseData = response.data;
-      console.log(responseData)
+      responseStatus = response.status;
+      }catch (error) {
+        responseData = error;
+      }
     }
 
     res.status(200).json({
@@ -32,9 +38,17 @@ app.post("/api/callEndpoint", async (req, res) => {
       response: responseData,
     });
   } catch (error) {
-    res.status(500).json({
+    let status = 500;
+    let errorMessage = "An error occurred";
+
+    if (error.response) {
+      status = error.response.status || 500;
+      errorMessage = error.response.data || "An error occurred";
+    }
+
+    res.status(401).json({
       status: "Error",
-      response: error.response?.data || "An error occurred",
+      response: errorMessage,
     });
   }
 });
