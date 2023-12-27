9cfb3b1c061cb854c87162e1b9827b08ef5c0e4c
sgupta19161 <sgupta19161@gmail.com>
2023-12-27 11:47:47 +0530

    Intital Commit

---
+++ myServer.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+
+const app = express();
+const port = process.env.PORT||3001;
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/api/callEndpoint", async (req, res) => {
+  const { method, fetchURL, data } = req.body;
+
+  try {
+    let responseData;
+    let responseStatus;
+
+    if (method === "GET") { 
+      const response = await axios.get(fetchURL);
+      responseData = response.data;
+      responseStatus = response.status
+    
+    } else if (method === "POST") {
+      const response = await axios.post(fetchURL, data);
+      responseData = response.data;
+      console.log(responseData)
+    }
+
+    res.status(200).json({
+      status: responseStatus,
+      response: responseData,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "Error",
+      response: error.response?.data || "An error occurred",
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
