24f2ee921c0bcb207ae2cb29d0e9f7a89d51584a
ShivamG86 <151511259+ShivamG86@users.noreply.github.com>
2023-12-28 14:37:58 +0530

    Update myServer.js

--- myServer.js
+++ myServer.js
@@ -9,31 +9,93 @@ app.use(express.json());
 app.use(cors());
 
 app.post("/api/callEndpoint", async (req, res) => {
-  const { method, fetchURL, data } = req.body;
-
+  const { method, fetchURL, data, key, value } = req.body;
+  console.log(data);
   try {
     let responseData;
     let responseStatus;
 
     if (method === "GET") {
       try {
-      const response = await axios.get(fetchURL);
-      responseData = response.data;
-      responseStatus = response.status;
-      }catch (error) {
-        responseData = error;
+        // let token;
+
+        // if (fetchURL === "https://repo-8qu2.onrender.com/studentServer/students") {
+        //   const tokenRes = await axios.get(`https://repo-8qu2.onrender.com/studentServer/getToken`);
+        //   token = tokenRes.data;
+        // }
+
+        const headers = {};
+        if (key && typeof key === "string" && key.trim() !== "") {
+          headers[key] = value;
+        }
+
+        const response = await axios.get(fetchURL, { headers });
+
+        responseData = response.data;
+        responseStatus = response.status;
+      } catch (error) {
+        responseData = error.response
+          ? error.response.data
+          : "An error occurred";
+        responseStatus = error.response ? error.response.status : 500;
       }
     } else if (method === "POST") {
       try {
-      const response = await axios.post(fetchURL, data);
-      responseData = response.data;
-      responseStatus = response.status;
-      }catch (error) {
-        responseData = error;
+        const headers = {};
+        if (key && typeof key === "string" && key.trim() !== "") {
+          headers[key] = value;
+        }
+
+        const response = await axios.post(fetchURL, data, { headers });
+        responseData =  response.data ;
+        responseData = data;
+        responseStatus = response.status;
+        
+      } catch (error) {
+        responseData = error.response
+          ? error.response.data
+          : "An error occurred";
+        responseStatus = error.response ? error.response.status : 500;
+      }
+    }else if (method === "PUT") {
+      try {
+        const headers = {};
+        if (key && typeof key === "string" && key.trim() !== "") {
+          headers[key] = value;
+        }
+
+        const response = await axios.put(fetchURL, data, { headers });
+        responseData =  response.data ;
+        responseData = data;
+        responseStatus = response.status;
+        
+      } catch (error) {
+        responseData = error.response
+          ? error.response.data
+          : "An error occurred";
+        responseStatus = error.response ? error.response.status : 500;
+      }
+    }else if (method === "DELETE") {
+      try {
+        const headers = {};
+        if (key && typeof key === "string" && key.trim() !== "") {
+          headers[key] = value;
+        }
+
+        const response = await axios.delete(fetchURL, data, { headers });
+        responseData =  response.data ;
+        responseData = data;
+        responseStatus = response.status;
+        
+      } catch (error) {
+        responseData = error.response
+          ? error.response.data
+          : "An error occurred";
+        responseStatus = error.response ? error.response.status : 500;
       }
     }
 
-    res.status(200).json({
+    res.status(responseStatus).json({
       status: responseStatus,
       response: responseData,
     });
@@ -46,8 +108,8 @@ app.post("/api/callEndpoint", async (req, res) => {
       errorMessage = error.response.data || "An error occurred";
     }
 
-    res.status(401).json({
-      status: "Error",
+    res.status(status).json({
+      status: status,
       response: errorMessage,
     });
   }
